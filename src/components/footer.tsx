a8ae6574e31c53a9fbc317058e8d24f28b415772
Bruno Bulnes <bulnes.bruno@gmail.com>
2025-02-23 16:29:18 -0300

    feat: set heart icon for footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,11 @@
+import { HeartIcon } from "@heroicons/react/24/solid";
+
 export default function Footer() {
   return (
     <footer className="py-6 mt-8">
-      <p className="text-center">Made with &hearts; by Bruno Bulnes</p>
+      <p className="flex items-center justify-center gap-1">
+        Made with <HeartIcon className="size-4" title="Love" /> by Bruno Bulnes
+      </p>
     </footer>
   );
 }
