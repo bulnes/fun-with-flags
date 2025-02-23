f5064ed52400b35b8c5bd8f49233edd0242f3ae5
Bruno Bulnes <bulnes.bruno@gmail.com>
2025-02-23 19:12:50 -0300

    feat: add custom select component

--- src/components/select.tsx
+++ src/components/select.tsx
@@ -1,4 +1,7 @@
-import { generateRandomId } from "@/utils/generators";
+"use client";
+
+import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
 
 interface SelectProps {
   options: string[];
@@ -11,28 +14,56 @@ export default function Select({
   options,
   selected,
   setSelected,
-  label,
 }: SelectProps) {
-  const selectId = generateRandomId("select-");
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleListOptionClick = (option: string) => {
+    setSelected(option);
+    setIsOpen(false);
+  };
+
+  const renderButtonIcon = () => {
+    if (isOpen) {
+      return <ChevronUpIcon className="size-4" />;
+    }
+
+    return <ChevronDownIcon className="size-4" />;
+  };
+
+  const renderListWithOptions = () => {
+    if (!isOpen) {
+      return null;
+    }
+
+    return (
+      <ul className="absolute w-full mt-2 bg-white border border-gray-300 rounded-lg shadow-sm">
+        {options.map((option) => (
+          <li key={option}>
+            <button
+              type="button"
+              className="w-full px-4 py-2 text-left hover:bg-gray-100 focus:outline-none"
+              onClick={() => handleListOptionClick(option)}
+            >
+              {option}
+            </button>
+          </li>
+        ))}
+      </ul>
+    );
+  };
 
   return (
-    <>
-      <div className="relative mb-2">
-        <label htmlFor={selectId} className="sr-only">
-          {label}
-        </label>
+    <div className="relative">
+      <button
+        type="button"
+        className="flex justify-between items-center w-full px-4 py-2 bg-white border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span>{selected}</span>
+        <span>{renderButtonIcon()}</span>
+      </button>
 
-        <select
-          id={selectId}
-          value={selected}
-          onChange={(e) => setSelected(e.target.value)}
-          className="w-full px-4 py-2 bg-white border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none pr-11"
-        >
-          {options.map((option) => (
-            <option key={option}>{option}</option>
-          ))}
-        </select>
-      </div>
-    </>
+      {renderListWithOptions()}
+    </div>
   );
 }
