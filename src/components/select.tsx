fa649ffb11425cd4f16276affb5c92d2d7d577c0
Bruno Bulnes <bulnes.bruno@gmail.com>
2025-03-04 16:40:43 -0300

    refact: use dependecy structure on state updates

--- src/components/select.tsx
+++ src/components/select.tsx
@@ -27,7 +27,7 @@ export default function Select({
       <button
         type="button"
         className="flex justify-between items-center w-full px-4 py-2 bg-white border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none"
-        onClick={() => setIsOpen(!isOpen)}
+        onClick={() => setIsOpen((isCurrentOpen) => !isCurrentOpen)}
       >
         <span>{selected}</span>
         <span>
